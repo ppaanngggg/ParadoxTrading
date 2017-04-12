d60862b85264f25d1084a95f1d162c1d2342e4e4
ppaanngggg <hantian.pang@gmail.com>
2017-04-12 15:26:51 +0800

    add tick sample and add tick

--- samples/tick_backtest.py
+++ samples/tick_backtest.py
@@ -2,27 +2,26 @@ import logging
 
 from ParadoxTrading.Engine import MarketEvent, SignalType, StrategyAbstract
 from ParadoxTrading.EngineExt import (BacktestEngine, BacktestMarketSupply,
-                                      BarBacktestExecution, BarPortfolio)
-from ParadoxTrading.Fetch import FetchGuoJinMin, RegisterGuoJinMin
+                                      TickBacktestExecution, TickPortfolio)
+from ParadoxTrading.Fetch import FetchGuoJinTick, RegisterGuoJinTick
 from ParadoxTrading.Indicator import HighBar, LowBar
 from ParadoxTrading.Utils import SplitIntoMinute
 
 
 class MyStrategy(StrategyAbstract):
     def init(self):
-        self.addMarketRegister(RegisterGuoJinMin(
+        self.addMarketRegister(RegisterGuoJinTick(
             _product='rb'
         ))
         self.split_rb = SplitIntoMinute(5)
-        self.highest_rb = HighBar('highprice')
-        self.lowest_rb = LowBar('lowprice')
+        self.highest_rb = HighBar('lastprice')
+        self.lowest_rb = LowBar('lastprice')
 
     def deal(self, _market_event: MarketEvent):
         logging.debug('Strategy({}) recv {}'.format(
             self.name, _market_event.toDict()))
 
-        if self.split_rb.addOne(_market_event.data) and len(
-                self.split_rb.getBarList()) > 1:
+        if self.split_rb.addOne(_market_event.data) and len(self.split_rb) > 1:
             # create a new 5 min bar
             self.highest_rb.addOne(
                 self.split_rb.getBarEndTimeList()[-2],
@@ -33,19 +32,19 @@ class MyStrategy(StrategyAbstract):
                 self.split_rb.getBarList()[-2]
             )
         if len(self.highest_rb.getAllData()):
-            cur_closeprice = _market_event.data['closeprice'][0]
+            lastprice = _market_event.data['lastprice'][0]
             last_highprice = self.highest_rb.getLastData()['high'][0]
             last_lowprice = self.lowest_rb.getLastData()['low'][0]
 
-            if cur_closeprice > last_highprice and self.getLastSignal() != SignalType.LONG:
+            if lastprice > last_highprice and self.getLastSignal() != SignalType.LONG:
                 self.addEvent(_market_event.symbol, SignalType.LONG)
                 logging.info('CLOSE: {}, HIGH: {}, LOW: {}'.format(
-                    cur_closeprice, last_highprice, last_lowprice
+                    lastprice, last_highprice, last_lowprice
                 ))
-            if cur_closeprice < last_lowprice and self.getLastSignal() != SignalType.SHORT:
+            if lastprice < last_lowprice and self.getLastSignal() != SignalType.SHORT:
                 self.addEvent(_market_event.symbol, SignalType.SHORT)
                 logging.info('CLOSE: {}, HIGH: {}, LOW: {}'.format(
-                    cur_closeprice, last_highprice, last_lowprice
+                    lastprice, last_highprice, last_lowprice
                 ))
 
 
@@ -53,15 +52,15 @@ logging.basicConfig(level=logging.INFO)
 
 strategy = MyStrategy('range_break')
 
-fetcher = FetchGuoJinMin()
-fetcher.psql_host = '192.168.4.102'
-fetcher.psql_user = 'ubuntu'
-fetcher.mongo_host = '192.168.4.102'
+fetcher = FetchGuoJinTick()
+# fetcher.psql_host = '192.168.4.102'
+# fetcher.psql_user = 'ubuntu'
+# fetcher.mongo_host = '192.168.4.102'
 
 market_supply = BacktestMarketSupply(
-    '20160101', '20160131', fetcher)
-execution = BarBacktestExecution(_commission_rate=0.001)
-portfolio = BarPortfolio()
+    '20160104', '20160105', fetcher)
+execution = TickBacktestExecution(_commission_rate=0.001)
+portfolio = TickPortfolio()
 
 engine = BacktestEngine()
 engine.addMarketSupply(market_supply)
@@ -71,4 +70,4 @@ engine.addStrategy(strategy)
 
 engine.run()
 
-portfolio.storeRecords('range_break')
+portfolio.storeRecords('range_break_tick')
