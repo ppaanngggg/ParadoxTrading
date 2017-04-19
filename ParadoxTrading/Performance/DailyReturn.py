2ca09a4a54e330d17518805f2481402415e96fa7
ppaanngggg <hantian.pang@gmail.com>
2017-04-19 17:38:39 +0800

    fix dailyreturn

--- ParadoxTrading/Performance/DailyReturn.py
+++ ParadoxTrading/Performance/DailyReturn.py
@@ -1,4 +1,5 @@
 import typing
+import logging
 
 from ParadoxTrading.Engine import ActionType, DirectionType, SignalType
 from ParadoxTrading.Performance.Utils import FetchRecord
@@ -46,14 +47,15 @@ def _daily_return(
     assert fill_dict.keys() <= settlement_dict.keys()
 
     for tradingday in sorted(settlement_dict.keys()):
-        if tradingday not in fill_dict.keys():
-            continue
-        for d in fill_dict[tradingday]:
-            cur_fund -= d['commission'][0]
-            if d['direction'][0] == DirectionType.BUY:
-                cur_fund -= d['price'][0] * d['quantity'][0]
-            if d['direction'][0] == DirectionType.SELL:
-                cur_fund += d['price'][0] * d['quantity'][0]
+        try:
+            for d in fill_dict[tradingday]:
+                cur_fund -= d['commission'][0]
+                if d['direction'][0] == DirectionType.BUY:
+                    cur_fund -= d['price'][0] * d['quantity'][0]
+                if d['direction'][0] == DirectionType.SELL:
+                    cur_fund += d['price'][0] * d['quantity'][0]
+        except KeyError as e:
+            logging.debug(e)
         ret.addDict({
             'tradingday': tradingday,
             'fund': cur_fund + settlement_dict[tradingday]['unfilled_fund'],
