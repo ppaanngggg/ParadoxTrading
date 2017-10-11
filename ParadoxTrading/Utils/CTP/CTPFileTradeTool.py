98734f77da74b295c2ce4d95411d36b48299faa1
ppaanngggg <hantian.pang@gmail.com>
2017-10-11 20:36:01 +0800

    adjust file trade tool

--- ParadoxTrading/Utils/CTP/CTPFileTradeTool.py
+++ ParadoxTrading/Utils/CTP/CTPFileTradeTool.py
@@ -1,12 +1,58 @@
 import configparser
 import csv
 import logging
+import typing
 from time import sleep
 
 import schedule
 
-from ParadoxTrading.Engine import DirectionType, ActionType
+from ParadoxTrading.Engine import ActionType, DirectionType
 from ParadoxTrading.Utils.CTP.CTPTraderSpi import CTPTraderSpi
+
+
+class OrderObj:
+    def __init__(
+            self, _index, _symbol, _quantity, _action, _direction,
+    ):
+        self.index = _index
+        self.symbol = _symbol
+        self.quantity = _quantity
+        self.action = _action
+        self.direction = _direction
+
+    def __repr__(self):
+        ret = 'INDEX: {}, SYMBOL: {}, QUANTITY: {}, ACTION: {}, DIRECTION: {}'
+        return ret.format(
+            self.index, self.symbol, self.quantity,
+            self.action, self.direction
+        )
+
+
+class FillObj:
+    def __init__(
+            self, _index, _symbol, _action, _direction
+    ):
+        self.index = _index
+        self.symbol = _symbol
+        self.action = _action
+        self.direction = _direction
+        self.quantity = 0.0
+        self.price = 0.0
+        self.commission = 0.0
+
+    def add(self, _quantity, _price, _commission):
+        tmp = self.quantity * self.price + _quantity * _price
+        self.quantity += _quantity
+        self.price = tmp / self.quantity
+        self.commission += _commission
+
+    def __repr__(self):
+        ret = 'INDEX: {}, SYMBOL: {}, ACTION: {}, DIRECTION: {}, ' \
+              'QUANTITY: {}, PRICE: {}, COMMISSION: {}'
+        return ret.format(
+            self.index, self.symbol, self.action, self.direction,
+            self.quantity, self.price, self.commission
+        )
 
 
 class CTPFileTradeTool:
@@ -26,6 +72,9 @@ class CTPFileTradeTool:
         self.trader: CTPTraderSpi = None
 
     def newTraderSpi(self):
+        """
+        create a new trader obj
+        """
         self.trader = CTPTraderSpi(
             self.config['TRADE']['ConPath'].encode(),
             self.config['TRADE']['Front'].encode(),
@@ -35,55 +84,64 @@ class CTPFileTradeTool:
         )
 
     def delTraderSpi(self):
+        """
+        release trader obj and reset arg
+        """
         self.trader.Release()
         del self.trader
         self.trader: CTPTraderSpi = None
 
-    def getRemainOrderTable(self):
-        ret = {}
+    def getOrderAndFillTable(self):
+        order_table: typing.Dict[int, OrderObj] = {}
+        fill_table: typing.Dict[int, FillObj] = {}
 
         try:
             order_file = open(self.order_csv_path)
         except FileNotFoundError:
             logging.warning('NO order csv found')
-            return ret
+            return order_table, fill_table
+
+        order_file.readline()
+        order_reader = csv.reader(order_file)
+
+        for order_line in order_reader:
+            index, instrument, action, direction, quantity = order_line
+            index = int(index)
+            direction = DirectionType.fromStr(direction)
+            action = ActionType.fromStr(action)
+            order_table[index] = OrderObj(
+                _index=index, _symbol=instrument, _quantity=int(float(quantity)),
+                _action=action, _direction=direction
+            )
+            fill_table[index] = FillObj(
+                _index=index, _symbol=instrument,
+                _action=action, _direction=direction,
+            )
+        # finish order csv
 
         try:
             fill_file = open(self.fill_csv_path)
         except FileNotFoundError:
             logging.warning('NO fill csv found')
-            fill_file = open(self.fill_csv_path, 'w')
-            fill_writer = csv.writer(fill_file)
-            fill_writer.writerow((
-                'Index', 'Symbol', 'Quantity',
-                'Action', 'Direction', 'Price', 'Commission'
-            ))
-            fill_file = open(self.fill_csv_path)
+            return order_table, fill_table
 
-        order_file.readline()
         fill_file.readline()
-        order_reader = csv.reader(order_file)
         fill_reader = csv.reader(fill_file)
 
-        for order in order_reader:
-            index, instrument, action, direction, quantity = order
-            direction = DirectionType.fromStr(direction)
-            action = ActionType.fromStr(action)
-            ret[int(index)] = {
-                'Symbol': instrument, 'Quantity': int(float(quantity)),
-                'Action': action, 'Direction': direction
-            }
-
-        for fill in fill_reader:
-            index = int(fill[0])
-            del ret[index]
+        for fill_line in fill_reader:
+            index, _, quantity, _, _, price, commission = fill_line
+            index = int(index)
+            fill_obj = fill_table[index]
+            fill_obj.quantity = int(float(quantity))
+            fill_obj.price = float(price)
+            fill_obj.commission = float(commission)
 
         order_file.close()
         fill_file.close()
 
-        return ret
+        return order_table, fill_table
 
-    def _trader_login(self):
+    def traderLogin(self):
         self.newTraderSpi()  # create ctp obj
 
         if not self.trader.Connect():  # connect front
@@ -97,7 +155,7 @@ class CTPFileTradeTool:
         sleep(1)
         return True
 
-    def _trader_all_instrument(self, _instrument_table: dict):
+    def traderAllInstrument(self, _instrument_table: dict):
         tmp = self.trader.ReqQryInstrument()  # all instrument
         if tmp is False:
             return False
@@ -106,109 +164,135 @@ class CTPFileTradeTool:
         sleep(1)
         return True
 
-    def _trader_send_order(
-            self, _index, _instrument_table, _order_table, _fill_table
+    def doTrade(
+            self, _order_obj: OrderObj, _fill_obj: FillObj,
+            _instrument_table, _commission_table
     ):
-        # order info
-        order_info = _order_table[_index]
-        # instrument info
-        inst_info = _instrument_table[order_info['Symbol']]
+        quantity_diff = int(_order_obj.quantity - _fill_obj.quantity)
+        if not quantity_diff > 0:  # order finished
+            return
+
+        inst_info = _instrument_table[_order_obj.symbol]
         # encode the bytes instrument for ctp
         instrument = inst_info['InstrumentID'].encode()
-        # commission rate
-        comm_info = self.trader.ReqQryInstrumentCommissionRate(instrument)
-        sleep(1)
-        if comm_info is False:
-            return False
-        # depth market data
-        market_info = self.trader.ReqQryDepthMarketData(instrument)
-        sleep(1)
-        if market_info is False:
-            return False
-        # limit price
-        price_diff = self.price_rate * inst_info['PriceTick']
-        if order_info['Direction'] == DirectionType.BUY:
-            price = market_info['AskPrice'] + price_diff
-        elif order_info['Direction'] == DirectionType.SELL:
-            price = market_info['BidPrice'] - price_diff
-        else:
-            raise Exception('unknown direction')
-        # send order
-        trade_info = self.trader.ReqOrderInsert(
-            instrument,
-            order_info['Direction'],
-            order_info['Action'],
-            order_info['Quantity'], price
-        )
-        sleep(1)
-        if trade_info is False:
-            return False
-        # !!! trade succeed !!!
-        comm_value = 0
-        if order_info['Action'] == ActionType.OPEN:
-            comm_value += comm_info['OpenRatioByMoney'] * trade_info['Price'] * \
-                          trade_info['Volume'] * inst_info['VolumeMultiple']
-            comm_value += comm_info['OpenRatioByVolume'] * trade_info['Volume']
-        elif order_info['Action'] == ActionType.CLOSE:
-            comm_value += comm_info['CloseRatioByMoney'] * trade_info['Price'] * \
-                          trade_info['Volume'] * inst_info['VolumeMultiple']
-            comm_value += comm_info['CloseRatioByVolume'] * trade_info['Volume']
-        else:
-            raise Exception('unknown action')
-        _fill_table[_index] = {
-            'Symbol': order_info['Symbol'],
-            'Quantity': trade_info['Volume'],
-            'Action': ActionType.toStr(order_info['Action']),
-            'Direction': DirectionType.toStr(order_info['Direction']),
-            'Price': trade_info['Price'],
-            'Commission': comm_value,
-        }
-        logging.info('!!! FILL {}: {} !!!'.format(
-            _index, _fill_table[_index]
+        # get commission from buffer
+        try:
+            comm_info = _commission_table[_order_obj.symbol]
+        except KeyError:  # fetch it if failed
+            comm_info = self.trader.ReqQryInstrumentCommissionRate(instrument)
+            sleep(1)
+            if comm_info is False:
+                return
+            _commission_table[_order_obj.symbol] = comm_info
+
+        for _ in range(quantity_diff):  # order one by one
+            # depth market data
+            market_info = self.trader.ReqQryDepthMarketData(instrument)
+            sleep(1)
+            if market_info is False:
+                continue
+            # limit price
+            price_diff = self.price_rate * inst_info['PriceTick']
+            if _order_obj.direction == DirectionType.BUY:
+                price = market_info['AskPrice'] + price_diff
+            elif _order_obj.direction == DirectionType.SELL:
+                price = market_info['BidPrice'] - price_diff
+            else:
+                raise Exception('unknown direction')
+            # send order
+            trade_info = self.trader.ReqOrderInsert(
+                instrument,
+                _order_obj.direction, _order_obj.action,
+                1, price
+            )
+            sleep(1)
+            if trade_info is False:
+                continue
+            # !!! trade succeed !!!
+            comm_value = 0
+            if _order_obj.action == ActionType.OPEN:
+                comm_value += comm_info['OpenRatioByMoney'] * trade_info['Price'] * \
+                              trade_info['Volume'] * inst_info['VolumeMultiple']
+                comm_value += comm_info['OpenRatioByVolume'] * trade_info['Volume']
+            elif _order_obj.action == ActionType.CLOSE:
+                comm_value += comm_info['CloseRatioByMoney'] * trade_info['Price'] * \
+                              trade_info['Volume'] * inst_info['VolumeMultiple']
+                comm_value += comm_info['CloseRatioByVolume'] * \
+                              trade_info['Volume']
+            else:
+                raise Exception('unknown action')
+            _fill_obj.add(
+                _quantity=trade_info['Volume'],
+                _price=trade_info['Price'],
+                _commission=comm_value,
+            )
+            logging.info('!!! FILL: {} !!!'.format(_fill_obj))
+
+    def writeFillTable(self, _fill_table: typing.Dict[int, FillObj]):
+        fill_file = open(self.fill_csv_path, 'w')
+        fill_writer = csv.writer(fill_file)
+        fill_writer.writerow((
+            'Index', 'Symbol', 'Quantity',
+            'Action', 'Direction', 'Price', 'Commission'
         ))
-        return True
+        for k, v in _fill_table.items():
+            if v.quantity == 0:  # skip not filled
+                continue
+            fill_writer.writerow((
+                k, v.symbol, int(v.quantity),
+                ActionType.toStr(v.action),
+                DirectionType.toStr(v.direction),
+                v.price, v.commission
+            ))
+        fill_file.close()
+
+    def checkOrderAndFill(self, _order_table, _fill_table):
+        for index in _order_table:
+            if _fill_table[index].quantity != _order_table[index].quantity:
+                return True
+        return False
 
     def tradeFunc(self):
         instrument_table = {}
-        order_table = self.getRemainOrderTable()
-        fill_table = {}
+        commission_table = {}
+        order_table, fill_table = self.getOrderAndFillTable()
+
+        if not self.checkOrderAndFill(order_table, fill_table):
+            return
+
+        # try to login
+        for i in range(self.retry_time):
+            logging.info('!!! TRY({}) to login !!!'.format(i))
+            if self.traderLogin():
+                break
+        else:
+            logging.warning('!!! LOGIN FAILED !!!')
+            return
+        # fetch all instruments
+        for i in range(self.retry_time):
+            logging.info('!!! TRY({}) to fetch all instruments !!!'.format(i))
+            if self.traderAllInstrument(instrument_table):
+                break
+        else:
+            logging.warning('!!! FETCH ALL INSTRUMENTS FAILED !!!')
+            return
 
         for i in range(self.retry_time):
             logging.info('!!! TRY ({})th TIME !!!'.format(i))
-            logging.info('!!! REMAIN ORDERS !!!')
-            for k, v in order_table.items():
-                logging.info('!!! ORDER {}: {} !!!'.format(k, v))
-            if len(order_table) == 0:
-                # no order left
+            for k in order_table:
+                self.doTrade(
+                    order_table[k], fill_table[k],
+                    instrument_table, commission_table
+                )
+            if not self.checkOrderAndFill(order_table, fill_table):
                 break
 
-            if not self._trader_login():
-                continue
-            if not self._trader_all_instrument(instrument_table):
-                continue
+        # release trader spi
+        self.delTraderSpi()  # free ctp obj
+        sleep(1)
 
-            indices = tuple(order_table.keys())
-            for index in indices:
-                if not self._trader_send_order(
-                        index, instrument_table, order_table, fill_table
-                ):
-                    continue
-
-                del order_table[index]
-
-            self.delTraderSpi()  # free ctp obj
-            sleep(1)
-
-        # write into fill csv
-        fill_file = open(self.fill_csv_path, 'a')
-        fill_writer = csv.writer(fill_file)
-        for k, v in fill_table.items():
-            fill_writer.writerow((
-                k, v['Symbol'], v['Quantity'],
-                v['Action'], v['Direction'],
-                v['Price'], v['Commission'],
-            ))
-        fill_file.close()
+        # writer fill table to file
+        self.writeFillTable(fill_table)
 
     def run(self):
         schedule.every().day.at("21:00").do(self.tradeFunc)
--- test/ctp_cmdline_test.py
+++ test/ctp_cmdline_test.py
@@ -1,3 +1,6 @@
+import logging
 from ParadoxTrading.Utils.CTP import CTPCmdLineTool
+
+logging.basicConfig(level=logging.INFO)
 
 CTPCmdLineTool('./config.ini').cmdloop()
