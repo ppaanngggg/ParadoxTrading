063af48904318b1adc5d9ecfc494c521841cf209
ppaanngggg <hantian.pang@gmail.com>
2018-02-08 13:20:20 +0800

    link import

--- ParadoxTrading/Indicator/__init__.py
+++ ParadoxTrading/Indicator/__init__.py
@@ -1,7 +1,12 @@
 from .Bar import OHLC, CloseBar, HighBar, LowBar, OpenBar, SumBar
 from .General import ATR, BIAS, CCI, EFF, EMA, KDJ, MA, MACD, MAX, MIN, RSI, \
     SAR, STD, AdaBBands, AdaKalman, BBands, Diff, FastBBands, FastMA, \
-    FastSTD, FastVolatility, Kalman, Momentum, Plunge, ReturnRate, SharpRate, \
-    SimMA, Volatility, ZigZag, LogReturn
+    FastSTD, FastVolatility, Kalman, LogReturn, Momentum, Plunge, ReturnRate, \
+    SharpRate, SimMA, Volatility, ZigZag
 from .Stop import ATRConstStop, ATRTrailingStop, RateConstStop, \
     RateTrailingStop, StepDrawdownStop, VolatilityTrailingStop
+
+try:
+    from .TSA import ARMAGARCH, GARCH
+except:
+    pass
